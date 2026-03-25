e8882401cbdd6684da1a3a2a925538614ed8c988
lalitmahato <lalitmahato11@gmail.com>
2026-03-25 06:50:52 +0545

    create operation

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todo.views import todo_list
+from todo.views import todo_list, create_todo
 
 urlpatterns = [
-    path('', todo_list) 
+    path('', todo_list, name="todo_list"),
+    path('create/', create_todo, name="crate_todo"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
 from todo.models import Todo
+from todo.forms import TodoForm
 
 # Create your views here.
 def hello_world(request):
@@ -26,8 +27,19 @@ def template_hello_world(request):
 
 
 def todo_list(request):
-    todo_query = Todo.objects.all()
+    todo_query = Todo.objects.filter(completion_status=False)
     context = {
         "todo_query": todo_query
     }
     return render(request, "todo/todo_list.html", context)
+
+def create_todo(request):
+    form = TodoForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid:
+            form.save()
+            return redirect("todo_list")
+    context = {
+        "form": form
+    }
+    return render(request, "todo/create_todo.html", context)
